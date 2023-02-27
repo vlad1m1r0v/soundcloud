d60c91789abb3595178431201a141f0b1f5b27ed
= <mypochtav@gmail.com>
2023-02-27 13:30:50 +0200

    changed returned result in music api

--- backend/src/music/music.service.ts
+++ backend/src/music/music.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AzureBlobService } from 'src/common/modules/azure/azure.service';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 import { Music } from './music.entity';
 import { EditMusicDto, UploadMusicDto } from './dtos';
 import { ErrorMessage } from 'src/common/enums';
@@ -28,33 +28,23 @@ export class MusicService {
       user: { id: userId },
       ...rest,
     });
-    await this.musicRepository
-      .createQueryBuilder()
-      .relation('genres')
-      .of(entity)
-      .addAndRemove(genres, []);
-    const uploadedMusic = this.musicRepository.save(entity);
-    return uploadedMusic;
+    await this.musicRepository.save(entity);
+    await this.updateGenres({ musicId: entity.id, genres });
+    const music = await this.selectMusic()
+      .where('music.id = :id', { id: entity.id })
+      .getOne();
+    return music;
   }
 
   async editMusic(musicId: string, dto: EditMusicDto) {
     try {
       const { genres = [], ...rest } = dto;
-      const previousGenres = await this.musicRepository
-        .createQueryBuilder()
-        .relation('genres')
-        .of({ id: musicId })
-        .loadMany();
-      await this.musicRepository
-        .createQueryBuilder()
-        .relation('genres')
-        .of({ id: musicId })
-        .addAndRemove(genres, previousGenres);
+      const previousGenres = await this.getMusicGenres(musicId);
+      await this.updateGenres({ musicId, previousGenres, genres });
       await this.musicRepository.update({ id: musicId }, { ...rest });
-      const music = await this.musicRepository
-        .createQueryBuilder('music')
-        .leftJoinAndSelect('music.genres', 'genre')
-        .getMany();
+      const music = await this.selectMusic()
+        .where('music.id = :id', { id: musicId })
+        .getOne();
       return music;
     } catch {
       throw new BadRequestException(ErrorMessage.INVALID_CREDETIALS);
@@ -71,5 +61,47 @@ export class MusicService {
     } catch {
       throw new BadRequestException(ErrorMessage.INVALID_CREDETIALS);
     }
+  }
+
+  private async getMusicGenres(musicId: string) {
+    const previousGenres = await this.musicRepository
+      .createQueryBuilder()
+      .relation('genres')
+      .of({ id: musicId })
+      .loadMany();
+    return previousGenres;
+  }
+
+  private async updateGenres({
+    musicId,
+    genres = [],
+    previousGenres = [],
+  }: {
+    musicId: string;
+    genres: any[];
+    previousGenres?: any[];
+  }) {
+    await this.musicRepository
+      .createQueryBuilder()
+      .relation('genres')
+      .of({ id: musicId })
+      .addAndRemove(genres, previousGenres);
+  }
+
+  private selectMusic(): SelectQueryBuilder<Music> {
+    return this.musicRepository
+      .createQueryBuilder('music')
+      .select([
+        'music.id',
+        'music.name',
+        'music.artist',
+        'music.url',
+        'users.id',
+        'users.firstName',
+        'users.lastName',
+        'users.avatar',
+      ])
+      .leftJoin('music.user', 'users')
+      .leftJoinAndSelect('music.genres', 'genre');
   }
 }
